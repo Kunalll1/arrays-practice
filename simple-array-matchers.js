9b69ff8f8384476c4d21d661abe3221c517dac02
kunalll1 <kunalkumarjha97@gmail.com>
2023-02-10 18:50:43 +0530

    solved 2nd question of Array Length

--- simple-array-matchers.js
+++ simple-array-matchers.js
@@ -14,3 +14,15 @@ function evenNum(num) {
   //   console.log(len.length);
 }
 evenNum([2, 3, 4, 6, 5, 7, 78, 9, 11, 12, 15]);
+
+// Write a function that takes an array of strings and returns the longest string in the array using the length property.
+
+function longestVel(string) {
+  let arr = []; // created an empty array to store the filter result arrays
+  for (let i = 0; i < string.length; i++) {
+    // created a loop to get all the value
+    arr.push(string[i].length); // pushing the filter list to Empty array with each string length.
+  }
+  //   console.log(Math.max(...arr));
+}
+longestVel(["kunal", "love", "react", "webDeveloper"]);
