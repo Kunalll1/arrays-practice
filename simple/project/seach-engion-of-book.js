75d0fd8f2964910d675292245186e2b192b71c50
kunalll1 <kunalkumarjha97@gmail.com>
2023-02-10 19:26:39 +0530

    setup the folder structures

---
+++ simple/project/seach-engion-of-book.js
@@ -0,0 +1,1 @@
+// A search engine for books: You can use an array to store book objects, where each object has properties like title, author, and ISBN. You can then use the includes method to search for books by title, author, or ISBN. You can also use the indexOf method to find the index of a specific book in the array.
